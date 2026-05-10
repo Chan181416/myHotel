6f8cd215af61474d882f4352912c1469d167bdf5
HelfgotC <HelfgotC@64482>
2026-05-10 18:41:59 +0300

    no

--- server/data/myHotelDbContext.cs
+++ server/data/myHotelDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
+using server.model;
 
 
 public class myHotelDbContext : DbContext
 {
-    public DbSet<RegisteredsDB> RegisteredsDBs { get; set; }
+    public DbSet<Registereds> Registereds { get; set; }
+    public DbSet<PricesList> pricesLists { get; set;}
     public DbSet<RoomsDB> RoomsDBs { get; set; }
     public DbSet<RegisteredsDB> registeredsDBs { get; set; }
     public DbSet<condition> conditions { get; set; }
