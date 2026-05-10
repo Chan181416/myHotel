b25be4ef1e4a21ff20ee2e993aae5e2fe3cd1782
hel&bli <tichnut71@8547900.org.il>
2026-05-10 18:37:50 +0300

    win

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0103864949663e137f6f3f92e8546354b5c99a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d160f7937cdd081f3d38fed2fa178b7ca9819e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
