96b3d38e7241a4ccc0a4a5280a980c901389d4f0
HelfgotC <HelfgotC@64482>
2026-05-11 12:22:14 +0300

    update-downled

--- server/Program.cs
+++ server/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<myHotelDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("myHotelDbContext"))); // עדכן כאן
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
