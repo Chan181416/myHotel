82961510083edb2a0c9b4c27ff02771b91db59a4
hel&bli <tichnut71@8547900.org.il>
2026-06-17 14:24:52 +0300

    errrrorrrr

--- server/server.Tests/UnitTest1.cs
+++ server/server.Tests/UnitTest1.cs
@@ -1,10 +1,10 @@
-﻿namespace server.Tests;
+﻿// namespace server.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+// public class UnitTest1
+// {
+//     [Fact]
+//     public void Test1()
+//     {
 
-    }
-}
+//     }
+// }
